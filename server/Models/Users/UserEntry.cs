5363b6a2000ffd63bb8e661f81c87da5884dba6c
unknown <rozanski.michal.94@gmail.com>
2020-05-01 17:16:28 +0200

    Create classes for Control DB Users

---
+++ server/Models/Users/UserEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.Users
+{
+    public class UserEntry
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public string PubKey { get; set; }
+    }
+}
---
+++ server/Models/Users/UsersRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace server.Models.Users
+{
+    public class UsersRepository : IDisposable
+    {
+        private readonly UsersContext db = new UsersContext(ConfigurationManager.ConnectionStrings["UsersConnectionStr"].ConnectionString);
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public UserEntry Get(string userName)
+        {
+            var query = from b in db.Entries
+                        where b.Name == userName
+                        select b;
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<UserEntry> GetAll()
+        {
+            var query = from b in db.Entries
+                        select b;
+
+            return query.AsEnumerable().Select(item =>
+                new UserEntry()
+                {
+                    Name = item.Name,
+                    Pass = item.Pass,
+                    PubKey = item.PubKey
+                });
+        }
+
+        public void Add(UserEntry user)
+        {
+            db.Entries.Add(user);
+            db.SaveChanges();
+        }
+
+        public void ModifyKey(string userName, string newPubKey)
+        {
+            var query = from b in db.Entries
+                        where b.Name == userName
+                        select b;
+
+            var user = query.FirstOrDefault();
+            user.PubKey = newPubKey;
+            db.SaveChanges();
+        }
+
+        public void Remove(string userName)
+        {
+            var query = from b in db.Entries
+                        where b.Name == userName
+                        select b;
+
+            var user = query.FirstOrDefault();
+            db.Entries.Remove(user);
+            db.SaveChanges();
+        }
+
+        public void RemoveAll()
+        {
+            db.Entries.RemoveRange(db.Entries);
+            db.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _log.Debug("UsersRepository Dispose");
+            throw new NotImplementedException();
+        }
+    }
+}
